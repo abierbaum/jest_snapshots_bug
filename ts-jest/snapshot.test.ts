00f4e0b3be9d66cd35d4b00f57d2c3879020fe88
Allen Bierbaum <abierbaum@gmail.com>
2021-10-14 15:43:32 -0500

    Add ts-jest example of the issue.

---
+++ ts-jest/snapshot.test.ts
@@ -0,0 +1,14 @@
+export function decTest(selector: string): Function {
+   return function testDec(proto: Function, name: string): void {
+      console.log('testing: ', proto.constructor.name, name, selector);
+   };
+}
+
+class TestClass {
+   @decTest('testing') value: string = '';
+}
+const inst = new TestClass();
+
+test('snaps', () => {
+   expect('value').toMatchInlineSnapshot(`"vue"`);
+})
